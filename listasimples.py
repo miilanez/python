19532201f830e8fda9b75fa282491b19714898bd
Gabriel Camiu <33701681+miilanez@users.noreply.github.com>
2021-01-06 15:02:25 -0300

    Update listasimples.py

--- listasimples.py
+++ listasimples.py
@@ -1,1 +1,8 @@
-starWarsMovies = ["the phantom menace", "clone wars", "revenge of the sith", "a new hope", "the empire strikes back", "the return of the jedi"]
+starWarsMovies = ["the phantom menace", 
+                  "clone wars", 
+                  "revenge of the sith", 
+                  "a new hope", 
+                  "the empire strikes back", 
+                  "the return of the jedi"]
+
+print (starWarsMovies)
