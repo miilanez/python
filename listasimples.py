43c4daf99aad4cdc546e766bb3115c1a541d2622
Gabriel Camiu <33701681+miilanez@users.noreply.github.com>
2021-01-07 09:15:10 -0300

    Add files via upload

--- listasimples.py
+++ listasimples.py
@@ -1,6 +1,8 @@
 starWarsMovies = ["the phantom menace", "clone wars", "revenge of the sith", "a new hope", "the empire strikes back", "the return of the jedi"]
 
-print(starWarsMovies)
+#Imprimir a lista de filmes em forma de lista
+for each_flick in starWarsMovies:
+    print(each_flick)
 
 print("Mas qual é o meu filme favorito sa lista?")
 print(starWarsMovies[2])
